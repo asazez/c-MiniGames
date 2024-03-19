c22aae0102ac422cb22a5c1dcc70360d6359755e
asazez <164041029+asazez@users.noreply.github.com>
2024-03-20 00:16:46 +0800

    Create tic-tak-toe.h

---
+++ tic-tak-toe.h
@@ -0,0 +1,8 @@
+// Function prototypes
+int boardState(int board, int pos);
+void printBoard(int board);
+int placePiece(int board, int pos, int p);
+int movePiece(int board, int from, int to, int p);
+int formLine(int board);
+int movecheckfrom(int board, int from, int p);          
+int movecheckto(int board, int to);                  
